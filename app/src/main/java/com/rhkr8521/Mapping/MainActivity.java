b95e8be1f885916029e4624edd3df516fe12c296
rhkr8521 <rhkr8521@gmail.com>
2024-02-28 12:39:33 +0900

    Initial Commit

---
+++ app/src/main/java/com/rhkr8521/Mapping/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rhkr8521.Mapping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
